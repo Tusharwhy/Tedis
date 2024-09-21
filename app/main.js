508ce198a9b25f5a0ad7be8af8a325d54b1b8bdd
Tusharwhy <tusharrajora29@gmail.com>
2024-09-21 23:31:35 +0530

    Creating Connection and code setup

---
+++ app/main.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+console.log("Initiated");
+
+const server = net.createServer((connection) => {
+  connection.write("Client connected to server\r\n");
+
+  connection.on("data", (data) => {
+    const message = data.toString().trim();
+    console.log("message: ", message);
+
+    if (message.includes("PING")) {
+      connection.write("+PONG\r\n");
+    }
+  });
+
+  connection.on("end", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(6379, "127.0.0.1", () => {
+  console.log("Tedis server is listening on port 6379");
+});
